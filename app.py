31897f38ba3595743bfd99fed84bdcc7010ccb5f
bituinronie <ronie.bituin@trainocate.com>
2022-11-20 12:45:52 +1100

    :100: integrated a new dataset

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ def recommend_program():
         #Algorithm here - Decision Tree
         #import libraries
         import numpy as np
-        import pandas as pd  
-        from sklearn.model_selection import train_test_split
+        import pandas as pd
         from sklearn.tree import DecisionTreeClassifier
 
         age = request.form.get('age')
@@ -33,62 +32,52 @@ def recommend_program():
         blood_pressure_diastolic = request.form.get('blood_pressure_diastolic')
         heart_rate = request.form.get('heart_rate')
         respiration = request.form.get('respiration')
-        print(request)
         
         DATA_CSV_FILE = pd.read_csv('ride_data_set.csv')
         DATA_CSV_FILE.isnull().sum()
 
         print(DATA_CSV_FILE)
         X = pd.DataFrame(np.c_[
-            DATA_CSV_FILE['min_age'],
-            DATA_CSV_FILE['max_age'],
-            DATA_CSV_FILE['low_systolic'],
-            DATA_CSV_FILE['low_diastolic'],
-            DATA_CSV_FILE['high_systolic'],
-            DATA_CSV_FILE['high_diastolic'],
-            DATA_CSV_FILE['heart_below'],
-            DATA_CSV_FILE['heart_above'],
-            DATA_CSV_FILE['respiration_below'],
+            DATA_CSV_FILE['age'],
+            DATA_CSV_FILE['systolic'],
+            DATA_CSV_FILE['diastolic'],
+            DATA_CSV_FILE['heart_rate'],
+            DATA_CSV_FILE['respiration_rate'],
             ],
-            columns = ['min_age',
-            'max_age',
-            'low_systolic',
-            'low_diastolic',
-            'high_systolic',
-            'high_diastolic',
-            'heart_below',
-            'heart_above',
-            'respiration_below',
+            columns = ['age',
+            'systolic',
+            'diastolic',
+            'heart_rate',
+            'respiration_rate',
             ])
         Y = DATA_CSV_FILE['allow_ride']
 
-
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=5)
-        print(X_train.shape)
-        print(X_test.shape)
-        print(Y_train.shape)
-        print(Y_test.shape)
-
         clf = DecisionTreeClassifier()
-        clf.fit(X_train, Y_train)
+        clf.fit(X, Y)
 
-        allow = clf.predict([[age, age, blood_pressure_systolic, blood_pressure_diastolic, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, heart_rate, respiration]])
+        allow = clf.predict([[age, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, respiration]])
         allow = allow[0]
 
         #Suggestions
+        X = pd.DataFrame(np.c_[
+            DATA_CSV_FILE['age'],
+            DATA_CSV_FILE['systolic'],
+            DATA_CSV_FILE['diastolic'],
+            DATA_CSV_FILE['heart_rate'],
+            DATA_CSV_FILE['respiration_rate'],
+            ],
+            columns = ['age',
+            'systolic',
+            'diastolic',
+            'heart_rate',
+            'respiration_rate',
+            ])
         Y = DATA_CSV_FILE['suggestion']
 
-
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=5)
-        print(X_train.shape)
-        print(X_test.shape)
-        print(Y_train.shape)
-        print(Y_test.shape)
-
         clf = DecisionTreeClassifier()
-        clf.fit(X_train, Y_train)
+        clf.fit(X, Y)
 
-        suggestion = clf.predict([[age, age, blood_pressure_systolic, blood_pressure_diastolic, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, heart_rate, respiration]])
+        suggestion = clf.predict([[age, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, respiration]])
         suggestion = suggestion[0]
 
         return jsonify({"allow":allow, "suggestions":suggestion})
