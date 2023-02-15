7a9b20b233e2d6c9ab5bb9025170ec25ba3f8915
blankspeedster <ronieB03@gmail.com>
2023-02-15 14:03:32 +1100

    :100: Update app.py for api call training

--- app.py
+++ app.py
@@ -10,65 +10,85 @@ app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-# CORS(app)
 
 @app.route('/', methods = ['GET'])
 def get_articles():
     return jsonify({"Hello":"World"})
 
-#Set the recommended program here
-recommendedProgram = "NA"
-
 @app.route('/get-suggestion', methods = ['GET', 'POST'])
 def recommend_program():
     if request.method == 'POST':
-        #Algorithm here - Decision Tree
-        #import libraries
-        import numpy as np
-        import pandas as pd
-        from sklearn.tree import DecisionTreeClassifier
+        try:
+            #import libraries
+            import joblib
+            import numpy as np
+            import pandas as pd
 
-        age = request.form.get('age')
-        blood_pressure_systolic = request.form.get('blood_pressure_systolic')
-        blood_pressure_diastolic = request.form.get('blood_pressure_diastolic')
-        heart_rate = request.form.get('heart_rate')
-        respiration = request.form.get('respiration')
-        
-        DATA_CSV_FILE = pd.read_csv('ride_data_set.csv')
-        DATA_CSV_FILE.isnull().sum()
+            # Get the data from the POST request.
+            age = request.form.get('age')
+            blood_pressure_systolic = request.form.get('blood_pressure_systolic')
+            blood_pressure_diastolic = request.form.get('blood_pressure_diastolic')
+            heart_rate = request.form.get('heart_rate')
+            respiration = request.form.get('respiration')
+            
+            DATA_CSV_FILE = pd.read_csv('ride_data_set.csv')
 
-        X = pd.DataFrame(np.c_[
-            DATA_CSV_FILE['age'],
-            DATA_CSV_FILE['systolic'],
-            DATA_CSV_FILE['diastolic'],
-            DATA_CSV_FILE['heart_rate'],
-            DATA_CSV_FILE['respiration_rate'],
-            ],
-            columns = ['age',
-            'systolic',
-            'diastolic',
-            'heart_rate',
-            'respiration_rate',
-            ])
-        Y = DATA_CSV_FILE['allow_ride']
+            # Load the model from the saved .pkl file
+            clf = joblib.load("trained_model.pkl")
 
-        clf = DecisionTreeClassifier()
-        clf.fit(X, Y)
+            allow = clf.predict([[age, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, respiration]])
+            allow = allow[0]
 
-        allow = clf.predict([[age, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, respiration]])
-        allow = allow[0]
+            #Suggestions
+            suggestions = DATA_CSV_FILE[DATA_CSV_FILE['allow_ride'] == allow]
+            suggestions = suggestions.head(1)
+            suggestion = suggestions['suggestion'].values[0]
 
-        #Suggestions
-        suggestions = DATA_CSV_FILE[DATA_CSV_FILE['allow_ride'] == allow]
-        suggestions = suggestions.head(1)
-        suggestion = suggestions['suggestion'].values[0]
-        # suggestion = suggestions._get_value(0, 'suggestion')
-
-        return jsonify({"allow":allow, "suggestions":suggestion})
+            return jsonify({"allow":allow, "suggestions":suggestion})
+        except Exception as e:
+            #return what is the error from the try block
+            return jsonify({"Error: ":str(e)})
     else:
         pass
         return jsonify({"Error: ":'Please submit the fields first.'})
 
+
+@app.route('/train-model', methods = ['GET'])
+def train_model():
+    #Algorithm here - Decision Tree
+    #import libraries
+    import numpy as np
+    import pandas as pd
+    from sklearn.tree import DecisionTreeClassifier
+    import joblib
+
+    # Load the data into a pandas dataframe
+    DATA_CSV_FILE = pd.read_csv('ride_data_set.csv')
+    DATA_CSV_FILE.isnull().sum()
+
+    X = pd.DataFrame(np.c_[
+        DATA_CSV_FILE['age'],
+        DATA_CSV_FILE['systolic'],
+        DATA_CSV_FILE['diastolic'],
+        DATA_CSV_FILE['heart_rate'],
+        DATA_CSV_FILE['respiration_rate'],
+    ],
+    columns = ['age',
+        'systolic',
+        'diastolic',
+        'heart_rate',
+        'respiration_rate',
+    ])
+    Y = DATA_CSV_FILE['allow_ride']
+
+    # Train the model
+    clf = DecisionTreeClassifier()
+    clf.fit(X, Y)
+
+    # Save the trained model as a .pkl file
+    joblib.dump(clf, "trained_model.pkl")
+
+    return jsonify({"message: ":"Model trained and saved as trained_model.pkl"}) 
 
 if __name__ == "__main__":
     app.run(debug=True)
